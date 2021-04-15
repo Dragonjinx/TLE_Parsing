d43214119ad8b34e2a73e580c6a9943d809445e3
Dragonjinx <abhilekh1@hotmail.com>
2021-04-15 23:00:12 +0200

    Added callback function to saving frames

--- simple_demo.py
+++ simple_demo.py
@@ -18,17 +18,15 @@ if __name__ == '__main__':
 
     saat = []
 
-    for stt in states:
+    for stt in states   :
         satel = oc.Satellite(stt, 30, 60)
         saat.append(satel)
 
     # sat_state1 = oc.State(2000)
-    # sat_state2 = oc.State(5000)
-    # Sat = oc.Satellite(sat_state1, 30, 60)
+    # sat_state2 = oc.State(2000, 1000, 1000, 1000)
+    # Sat1 = oc.Satellite(sat_state1, 30, 60)
     # Sat2 = oc.Satellite(sat_state2, 30, 60)
-    # Sat.animate_sat_orb()
-    # Sat2.animate_sat_orb()
-    # plot_orbits([Sat, Sat2])
-    # saat = [Sat, Sat2]
+    # saat = [Sat1, Sat2]
     anime = oc.animate_orbits(saat, factor=10)
-    # anime.save("single.gif", writer='imagemagick', fps=60)
+    # Uncomment at your own risk! 
+    # anime.save("try.gif", writer='imagemagick', fps=60, progress_callback= lambda i, n: print(f'Saving frame {i} of {n}'))
