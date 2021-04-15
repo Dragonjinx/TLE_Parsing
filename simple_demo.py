6781c550459be858b6c9e56f8f8cc567429f8fa3
Dragonjinx <abhilekh1@hotmail.com>
2021-04-15 21:59:28 +0200

    Improved how satellite.propagate_orbit() handles collitions with orbit body
    	modified:   orbit_classes.py
    	changed from line 86 to better handle collision with orbit body
    
    	modified:   simple_demo.py
    	changed to simulate 5 random satellites

--- simple_demo.py
+++ simple_demo.py
@@ -1,13 +1,34 @@
 import orbit_classes as oc
+import random
 
 if __name__ == '__main__':
-    sat_state1 = oc.State(2000)
-    sat_state2 = oc.State(5000)
-    Sat = oc.Satellite(sat_state1, 30, 60)
-    Sat2 = oc.Satellite(sat_state2, 30, 60)
+
+    states = []
+
+    for i in range(5):
+        height = random.randint(1, 100) * 100
+        x = random.randint(1, 10) * 100
+        y = random.randint(1, 10) * 100
+        z = random.randint(1, 10) * 100
+        vx = random.randint(1, 5)
+        vy = random.randint(1, 5)
+        vz = random.randint(1, 5)
+        st = oc.State(height, x, y, z, vx, vy, vz)
+        states.append(st)
+
+    saat = []
+
+    for stt in states:
+        satel = oc.Satellite(stt, 30, 60)
+        saat.append(satel)
+
+    # sat_state1 = oc.State(2000)
+    # sat_state2 = oc.State(5000)
+    # Sat = oc.Satellite(sat_state1, 30, 60)
+    # Sat2 = oc.Satellite(sat_state2, 30, 60)
     # Sat.animate_sat_orb()
     # Sat2.animate_sat_orb()
     # plot_orbits([Sat, Sat2])
-    saat = [Sat, Sat2]
-    anime = oc.animate_orbits(saat)
+    # saat = [Sat, Sat2]
+    anime = oc.animate_orbits(saat, factor=10)
     # anime.save("single.gif", writer='imagemagick', fps=60)
