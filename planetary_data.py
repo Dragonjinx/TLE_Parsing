17f8a01a5967aa4bbdceb1dd3a77b915561eb938
Dragonjinx <abhilekh1@hotmail.com>
2021-04-16 20:27:48 +0200

    Added Gravitational constant definition

--- planetary_data.py
+++ planetary_data.py
@@ -1,5 +1,8 @@
+G_Meters = 6.67408e-11
+G_Km = G_Meters * 10**-9
+
 earth = {
-    'radius': 6860,
-    'mass':69,
-    'Mu':398600    
+    'radius': 6378, #Km
+    'mass':5.92e24, #Kg
+    'Mu': 398600  # (5.97e24 * G_Km) # Km^3 / s^2     
 }
